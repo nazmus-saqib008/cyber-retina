c58662e44df6cd8554202734829db64fd249104e
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2023-09-02 10:14:30 +0530

    added requirements.txt and changes done in app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import streamlit as st
+import tensorflow as tf
 import numpy as np
+import keras
+# import cv2
 from keras.models import load_model
 from PIL import Image
 
-MODEL_PATH= "model/k_fold_model.h5"
-model = load_model(MODEL_PATH)
+MODEL_PATH = "model/k_fold_model.h5"
+model = load_model(MODEL_PATH, custom_objects={})
 classes = ["Normal","Cataract","Diabetes","Glaucoma","Hypertension","Myopia","Age_Issues","Other"]
 
-
-def predict(image):
-    image = image.resize((224, 224))
-    image = np.array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
-
-    predictions = model.predict(image)
-    predicted_class = np.argmax(predictions[0])
-
-    return classes[predicted_class]
+def preprocess_image(image):
+    # Preprocess the image as needed (resize, normalization, etc.)
+    # Example:
+    resized_image = image.resize((224, 224))
+    array_image = np.array(resized_image)
+    normalized_image = array_image / 255.0  # Normalize pixel values
+    return normalized_image
 
 im = Image.open('elements/logo.jpeg')
 st.set_page_config(
@@ -37,9 +37,14 @@ if st.button("Predict"):
     if image_file:
         image = Image.open(image_file)
         st.write('')
-        prediction = predict(image)
-        st.success(f'Prediction: {prediction}')
-        st.image(image, caption='Uploaded Image',use_column_width=True)
+        preprocessed_image = preprocess_image(image)
+
+        image_expand = np.expand_dims(preprocessed_image, axis=0)
+
+        predictions = model.predict(np.expand_dims(preprocessed_image, axis=0))
+        predicted_class = np.argmax(predictions)
+        st.success(f'Prediction: {classes[predicted_class]}')
+        st.image(image, caption='Uploaded Image', use_column_width=True)
 
     else:
         st.error("No file selected")
